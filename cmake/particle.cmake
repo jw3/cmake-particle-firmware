54316c6b17001a3690bddf8988c233c6813afe55
John Wass <wassj@ctc.com>
2018-03-19 17:25:48 -0400

    move build config into module to support multiple apps

---
+++ cmake/particle.cmake
@@ -0,0 +1,13 @@
+function(add_particle_app name)
+    file(RELATIVE_PATH TARGET_DIR ${FIRMWARE_DIR}/main ${CMAKE_CURRENT_BINARY_DIR}/${name})
+
+    set(MAKE_ARGS
+        PLATFORM=${PLATFORM}
+        TARGET_DIR=${TARGET_DIR}
+        GCC_ARM_PATH=${GCC_ARM_PATH}
+        APPDIR=${CMAKE_CURRENT_SOURCE_DIR}/${name})
+
+    add_custom_target(${name} ALL
+                      COMMAND make ${MAKE_ARGS}
+                      WORKING_DIRECTORY ${FIRMWARE_DIR})
+endfunction(add_particle_app)
