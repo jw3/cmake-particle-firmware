58f3998ae7e1ea8d814df2d8d4eef8d888351e8f
John Wass <wassj@ctc.com>
2018-04-05 02:12:22 -0400

    prevent issues with repatching by doing a dry run first

--- patches/apply.sh
+++ patches/apply.sh
@@ -5,6 +5,9 @@ readonly proj="${root}/${1}"
 
 if [[ -d ${proj} ]]; then
     for p in $(ls ${proj}/*.patch 2> /dev/null); do
-      patch --binary -p 1 -i ${p}
+      patch --dry-run -N --silent --binary -p 1 -i ${p}
+      if [ $? -eq 0 ]; then
+        patch --binary -p 1 -i ${p}
+      fi
     done
 fi
