4375b1cdf517547e012abf31f1103e9134c2f4b8
John Wass <wassj@ctc.com>
2018-04-10 18:56:13 -0400

    improve the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then echo "usage: build <platform>"; exit 1; fi
+
+readonly platform="$1"
+
 rm -rf build
 mkdir build
 cd build
-cmake ..
-make v=1
+cmake .. -DPLATFORM="$platform"
+make
