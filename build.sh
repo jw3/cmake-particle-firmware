8fda11e8d2749e7a625d9997acb00b5a82d20959
John Wass <wassj@ctc.com>
2018-04-10 22:05:38 -0400

    update build script to support quick builds

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then echo "usage: build <platform>"; exit 1; fi
+if [ -z "$1" ]; then echo "usage: build.sh <platform>"; exit 1; fi
 
 readonly platform="$1"
+readonly builddir="build-$platform"
 
-rm -rf build
-mkdir build
-cd build
+if [[ "$2" != "quick" ]]; then
+  rm -rf "$builddir"
+  mkdir "$builddir"
+fi
+
+if [ ! -d "$builddir" ]; then mkdir "$builddir"; fi
+
+cd "$builddir"
 cmake .. -DPLATFORM="$platform"
-make
+make -j1
