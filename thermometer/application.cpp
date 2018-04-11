a5d0f62180758ee990d2c9ce2661dae333f18588
John Wass <wassj@ctc.com>
2018-04-11 17:01:21 -0400

    reformat

--- thermometer/application.cpp
+++ thermometer/application.cpp
@@ -10,24 +10,22 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-void setup(void)
-{
-    // start serial port
-    Serial.begin(9600);
-    Serial.println("Dallas Temperature IC Control Library Demo");
+void setup(void) {
+   // start serial port
+   Serial.begin(9600);
+   Serial.println("Dallas Temperature IC Control Library Demo");
 
-    // Start up the library
-    sensors.begin();
+   // Start up the library
+   sensors.begin();
 }
 
-void loop(void)
-{
-    // call sensors.requestTemperatures() to issue a global temperature
-    // request to all devices on the bus
-    Serial.print("Requesting temperatures...");
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    Serial.println("DONE");
+void loop(void) {
+   // call sensors.requestTemperatures() to issue a global temperature
+   // request to all devices on the bus
+   Serial.print("Requesting temperatures...");
+   sensors.requestTemperatures(); // Send the command to get temperatures
+   Serial.println("DONE");
 
-    Serial.print("Temperature for the device 1 (index 0) is: ");
-    Serial.println(sensors.getTempCByIndex(0));
+   Serial.print("Temperature for the device 1 (index 0) is: ");
+   Serial.println(sensors.getTempCByIndex(0));
 }
