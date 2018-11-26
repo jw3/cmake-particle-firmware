cd4a046fa8c1e729621db55c30b6aa663ad2d404
John Wass <wassj@ctc.com>
2018-11-25 21:39:25 -0500

    set firmware version from build script

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]; then echo "usage: build.sh <platform>"; exit 1; fi
+if [[ -z "$1" ]]; then echo "usage: build.sh <platform>"; exit 1; fi
 
 readonly platform="$1"
 readonly builddir="build-$platform"
+
+if [[ -z "$FIRMWARE_VERSION" ]]; then echo "FIRMWARE_VERSION is required"; exit 1; fi
+readonly firmware="$FIRMWARE_VERSION"
 
 if [[ "$2" != "quick" ]]; then
   rm -rf "$builddir"
   mkdir "$builddir"
 fi
 
-if [ ! -d "$builddir" ]; then mkdir "$builddir"; fi
+if [[ ! -d "$builddir" ]]; then mkdir "$builddir"; fi
 
 cd "$builddir"
-cmake .. -DPLATFORM="$platform"
+cmake .. -DPLATFORM="$platform" -DFIRMWARE_VERSION="$firmware"
 make -j1
